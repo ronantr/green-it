e23d0c4a8e4ece35ec65ad386d6c07bb7ce0c99f
pthushanth <pthushanth97@gmail.com>
2023-03-16 15:17:52 +0100

    Create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
+import { Routes, Route, Router } from "@solidjs/router";
+import Product from './pages/Product';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+    <Routes>
+      <Route path="/" component={Home} />
+      <Route path="/home" component={Home} />
+      <Route path="/product" component={Product} />
+    </Routes>
+    </Router>
+    </>
   );
 }
 
---
+++ src/pages/Product.jsx
@@ -0,0 +1,7 @@
+export default function Product() {
+    return <h1>Modele classe A </h1>;
+}
+
+
+
+
