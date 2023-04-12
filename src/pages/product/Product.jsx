30fd94e41decefc2633b383f8d49f911a05b61cd
pthushanth <pthushanth97@gmail.com>
2023-04-12 18:49:55 +0200

    Change car color

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,62 +1,110 @@
-import { A } from "@solidjs/router";
-
+import { createEffect, createSignal } from "solid-js";
+import "./Product.css";
+import "./style.css";
 
 export default function Product() {
-    return (
-        <div>
-            <div class="hero is-fullheight has-background">
-                <img alt="Audi" class="hero-background is-transparent" src="https://www.audi.fr/content/dam/nemo/fr/Gamme/e-tron-GT/RS-e-tron-GT/refonte/header/1920x1920_RSetronGT_header_20092022.jpg?imwidth=1440"/>
-                <div class="hero-body">
-                    <div class="container is -flex is-flex-direction-column is-justify-content-space-between  has-text-centered">
-                        <h1 class="title is-1 has-text-white">Audi</h1>
-                        <h2 class="subtitle is-3 has-text-white">RS e-tron GT</h2>
-                    </div>
-                </div>
-                
+  const [color, setColor] = createSignal("black");
 
-                <div class="container is-flex is-flex-direction-row is-align-items-end has-text-centered p-3 has-text-white" style={{ marginBottom: '10px' }}>
-                    <div class="statistic mx-6">
-                        <div class="title is-4 has-text-white">495 km</div>
-                        <div class="title is-7 has-text-white">Autonomie (WLTP)</div>
-                    </div>
-                    <div class="statistic mx-6">
-                        <div class="title is-4 has-text-white">598 ch</div>
-                        <div class="title is-7 has-text-white">Puissance</div>
-                    </div>
-                    <div class="statistic mx-6">
-                        <div class="title is-4 has-text-white">3,3 s</div>
-                        <div class="title is-7 has-text-white">0 à 100km/h</div>
-                    </div>
-                    <div class="statistic mx-6">
-                        <div class="title is-4 has-text-white">245 km/h</div>
-                        <div class="title is-7 has-text-white">Vitesse maximale</div>
-                    </div>
-                </div>
+  const changeColor = (e) => {
+    let color = e.target ? e.target.value : e;
+    console.log("test", color);
+    setColor(color);
+  };
+  const options = [{ color: "red" }, { color: "green" }, { color: "pink" }];
 
-            </div>
-            <style jsx>{`
-        @media (max-width: 768px) {
-            .container {
-                flex-wrap: wrap;
-                justify-content: center;
-            }
-            .statistic {
-                margin-bottom: 10px;
-            }
-        }
-    `}</style>
-
-            <div class="hero is-fullheight has-background">
-                <img alt="Audi" class="hero-background is-transparent" src="https://images.caricos.com/a/audi/2020_audi_rs6_avant/images/2560x1440/2020_audi_rs6_avant_16_2560x1440.jpg"/>
-                <div class="hero-body">
-                    <div class="container is -flex is-flex-direction-column is-justify-content-space-between  has-text-centered">
-                    </div>
-                </div>
-                {/* create content with a div on the right and the text on the right */}
-                
-            </div>
+  return (
+    <>
+      <div class="hero is-fullheight has-background">
+        <img
+          alt="Audi"
+          class="hero-background is-transparent"
+          src="https://www.bauerparis.fr/wp-content/uploads/2020/01/RS7-Sportback-2020-4.webp"
+        />
+        <div class="hero-body">
+          <div class="container is -flex is-flex-direction-column is-justify-content-space-between  has-text-centered">
+            <h1 class="title is-1 has-text-white">Audi</h1>
+            <h2 class="subtitle is-3 has-text-white">RS7</h2>
+          </div>
         </div>
-        
-    );
 
+        <div
+          class="container is-flex is-flex-direction-row is-align-items-end has-text-centered p-3 has-text-white"
+          style={{ marginBottom: "10px" }}
+        >
+          <div class="statistic mx-6">
+            <div class="title is-4 has-text-white">630 ch</div>
+            <div class="title is-7 has-text-white">Puissance</div>
+          </div>
+          <div class="statistic mx-6">
+            <div class="title is-4 has-text-white">3,4 s</div>
+            <div class="title is-7 has-text-white">0 à 100km/h</div>
+          </div>
+          <div class="statistic mx-6">
+            <div class="title is-4 has-text-white">280 km/h</div>
+            <div class="title is-7 has-text-white">Vitesse maximale</div>
+          </div>
+        </div>
+      </div>
+
+      {/* --------------------------------------------- */}
+      <div class="container my-4">
+        <div class="feature-wrap">
+          <h2 class="car-color-picker">
+            CHOOSE A COLOR:
+            <br />
+            <input
+              type="color"
+              value={color}
+              id="color-picker"
+              class="button is-rounded m-2"
+              onChange={changeColor}
+            />
+            {options.map((option) => (
+              <span
+                class="button is-rounded m-2"
+                style={{ "background-color": option.color }}
+                onClick={() => changeColor(option.color)}
+              ></span>
+            ))}
+          </h2>
+          <div
+            class="car-wrap"
+            id="car-wrap"
+            style={{ "background-color": color() }}
+          >
+            <img
+              // src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/13764/color-change-car-base.png"
+              src="/src/assets/images/color-change-car-base.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <div class="hero is-fullheight has-background">
+        <img
+          alt="Audi"
+          class="hero-background is-transparent"
+          src="https://images.caricos.com/a/audi/2020_audi_rs6_avant/images/2560x1440/2020_audi_rs6_avant_16_2560x1440.jpg"
+        />
+        <div class="hero-body">
+          <div class="container is -flex is-flex-direction-column is-justify-content-space-between  has-text-centered">
+            <div class="col-md-6 order-md-2">
+              <div class="content">
+                <h2 class="title is-4 has-text-white">
+                  {" "}
+                  Puissance et luxe en un seul véhicule
+                </h2>
+                <p class="has-text-white">
+                  La Audi RS7 Sportback est une voiture de luxe familiale dotée
+                  d'un moteur V8 bi-turbo de 600 chevaux, capable d'accélérer de
+                  0 à 100 km/h en seulement 3,6 secondes.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* ------------------ */}
+    </>
+  );
 }
