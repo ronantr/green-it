757c18015ce20254d4270d46da2f6e132b01614f
Kurunchi CHANDRA <ckurunchi@gmail.com>
2023-04-14 00:23:48 +0200

    add video on home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -52,11 +52,11 @@ function Home() {
         </section>
 
         <section class="section has-background-black">
-        <div class="container mt-4 mb-4">
+          <div class="container mt-4 mb-4">
             <div class="columns is-vcentered">
               <div class="column is-6 ">
                 <h2 class="title is-2 has-text-left has-text-white">La recharge de votre Audi électrique simplifiée</h2>
-                <p className ="has-text-white"> Audi vous offre des solutions de recharge sur-mesure. Accédez à l’un des plus vastes réseaux de recharge publique en Europe avec la carte Audi charging et profitez des avantages d’une borne installée à domicile avec ZEborne ou en entreprise avec Zeplug.</p>
+                <p className="has-text-white"> Audi vous offre des solutions de recharge sur-mesure. Accédez à l’un des plus vastes réseaux de recharge publique en Europe avec la carte Audi charging et profitez des avantages d’une borne installée à domicile avec ZEborne ou en entreprise avec Zeplug.</p>
               </div>
               <div class="column is-6">
                 <img src="https://www.audi.fr/dam/nemo/fr/Teasers-Homepage/2023/framed-1-1/885x885_recharge_31032023.jpg?imwidth=500" alt="Your image" />
@@ -70,11 +70,25 @@ function Home() {
                 <img src="https://www.audi.fr/dam/nemo/fr/Teasers-Homepage/2023/framed-1-1/885x885_myAudi_31032023.jpg?imwidth=500" alt="Your image" />
               </div>
               <div class="column is-6">
-              <h2 class="title is-2 has-text-left has-text-white">Un univers de privilèges pour les passionnés d’Audi</h2>
-              <p className ="has-text-white"> myAudi vous fait vivre une relation privilégiée avec Audi grâce à un ensemble de services exclusifs pour profiter pleinement de toutes les fonctionnalités connectées de votre Audi.</p>
+                <h2 class="title is-2 has-text-left has-text-white">Un univers de privilèges pour les passionnés d’Audi</h2>
+                <p className="has-text-white"> myAudi vous fait vivre une relation privilégiée avec Audi grâce à un ensemble de services exclusifs pour profiter pleinement de toutes les fonctionnalités connectées de votre Audi.</p>
               </div>
             </div>
           </div>
+          
+          <div class="container mt-4 mb-4">
+        <div class="columns is-vcentered">
+          <div class="column is-6 ">
+            <h2 class="title is-2 has-text-left has-text-white">La recharge rapide de votre voiture électrique</h2>
+            <p className="has-text-white">Audi a développé des batteries plus performantes, des systèmes de refroidissement innovants et des solutions de recharge ultra-rapides pour accélérer le temps de charge de votre voiture électrique Audi</p>
+          </div>
+          <div class="column is-6">
+            <video width="640" height="360" muted controls preload="none">
+              <source src="./src/assets/videos/Audi_TempsCharge_30sec_16x9_1.webm" type="video/mp4"/>
+            </video>
+          </div>
+        </div>
+      </div>
         </section>
 
         <section className="section my-3">
@@ -121,10 +135,10 @@ function Home() {
                       />
                     </figure>
                     <div className="small-image">
-                    <figure className="image">
-                      <img src="./src/assets/images/A3side.avif" alt="Small Image" />
-                    </figure>
-                  </div>
+                      <figure className="image">
+                        <img src="./src/assets/images/A3side.avif" alt="Small Image" />
+                      </figure>
+                    </div>
                   </div>
                   <div className="card-content">
                     <div className="content">
@@ -147,16 +161,16 @@ function Home() {
                       />
                     </figure>
                     <div className="small-image">
-                    <figure className="image">
-                      <img src="./src/assets/images/sideinconnue.avif" alt="Small Image" />
-                      
-                    </figure>
-                  </div>
+                      <figure className="image">
+                        <img src="./src/assets/images/sideinconnue.avif" alt="Small Image" />
+
+                      </figure>
+                    </div>
                   </div>
                   <div className="card-content">
                     <div className="content">
                       <h3 className="title is-3 has-text-white is-relative" >Audi A8</h3>
-                      
+
                       <button className="button is-primary is-inverted is-outlined">
                         Configurer
                       </button>
